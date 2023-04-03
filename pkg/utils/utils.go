46e3a265c6be48aae4c5b3f8ab64f5c018cd2372
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-04 01:08:25 +1200

    Add utility function to check if an element is in a slice

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,10 @@
 package utils
+
+func IsElementInSlice[T comparable](slice []T, element T) bool {
+	for _, sliceElement := range slice {
+		if sliceElement == element {
+			return true
+		}
+	}
+	return false
+}
