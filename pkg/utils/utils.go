bfd92d341e179425abd69c0dcf98b533eb0dee26
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-05 00:33:49 +1200

    Add a shuffle implemenetion to the utils function

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"math"
+
+	"golang.org/x/exp/rand"
+)
+
 func IsElementInSlice[T comparable](slice []T, element T) bool {
 	for _, sliceElement := range slice {
 		if sliceElement == element {
@@ -8,3 +14,11 @@ func IsElementInSlice[T comparable](slice []T, element T) bool {
 	}
 	return false
 }
+
+// Shuffles the given list
+func ShuffleSlice[T comparable](randomGenerator *rand.Rand, slice []T) {
+	randomGenerator.Shuffle(len(slice), func(i int, j int) {
+		slice[i], slice[j] = slice[j], slice[i]
+	})
+}
+
