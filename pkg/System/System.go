08e8f393072c08ef117d233fffe8ca60202bf0cb
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-06 22:22:11 +1200

    Add NumAgentsPerSimulation to system interface

--- pkg/System/System.go
+++ pkg/System/System.go
@@ -8,6 +8,7 @@ import (
 type System interface {
 	NumPercepts() int
 	NumActions() int
+	NumAgentsPerSimulation() int
 	ScoreFunction(systemstate.StateHistory) float64
 	InitializeState() systemstate.SystemState
 	AdvanceState(systemstate.SystemState, []agent.AgentAction) systemstate.SystemState
