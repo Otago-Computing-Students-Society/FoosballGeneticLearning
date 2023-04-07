0a1c92e1a66f48a814527a51244080a0bea271ed
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-08 01:13:44 +1200

    Refactor AdvanceState to return a pointer to systemstate

--- pkg/System/System.go
+++ pkg/System/System.go
@@ -10,6 +10,6 @@ type System interface {
 	NumActions() int
 	NumAgentsPerSimulation() int
 	ScoreFunction(systemstate.StateHistory) float64
-	AdvanceState(systemstate.SystemState, []agent.AgentAction) systemstate.SystemState
 	InitializeState() *systemstate.SystemState
+	AdvanceState(systemstate.SystemState, []agent.AgentAction) *systemstate.SystemState
 }
