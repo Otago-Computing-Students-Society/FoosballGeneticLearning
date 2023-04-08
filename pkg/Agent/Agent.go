032f11a320155eae72e468a94ed548b5d362b12c
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-09 01:58:42 +1200

    Refactor AgentAction and AgentScore AGAIN
    Now removing both structs entirely - self documentation achieved via method names instead and directly return mat.VecDense

--- pkg/Agent/Agent.go
+++ pkg/Agent/Agent.go
@@ -4,15 +4,8 @@ import (
 	systemstate "OCSS/FoosballGeneticLearning/pkg/SystemState"
 
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat/distuv"
 )
-
-type AgentAction struct {
-	Action *mat.VecDense
-}
-
-type AgentScore struct {
-	Score float64
-}
 
 type Agent struct {
 	Chromosome   *mat.Dense
@@ -28,12 +21,30 @@ func NewAgent(chromosome *mat.Dense) *Agent {
 	}
 }
 
-func (agent *Agent) GetAction(stateVector *mat.VecDense) AgentAction {
-func (agent *Agent) GetAction(stateVector *mat.VecDense) *AgentAction {
+// Return a new agent with a unit Gaussian random chromosome
+//
+// TODO(hayden): Actually implement this method!
+func NewRandomGaussianAgent(numActions int, numPercepts int) *Agent {
+	unitNormal := distuv.UnitNormal
+	chromosomeData := make([]float64, numActions*numPercepts)
+	for index := range chromosomeData {
+		chromosomeData[index] = unitNormal.Rand()
+	}
+	chromosome := mat.NewDense(numActions, numPercepts, chromosomeData)
+	return NewAgent(chromosome)
+}
+
+func (agent *Agent) GetAction(stateVector *mat.VecDense) *mat.VecDense {
 	numActions, _ := agent.Chromosome.Dims()
 	actionVector := mat.NewVecDense(numActions, nil)
 	actionVector.MulVec(agent.Chromosome, stateVector)
-	return &AgentAction{
-		Action: actionVector,
+	return actionVector
+}
+
+func GetAllAgentActions(agents []*Agent, stateVector *mat.VecDense) []*mat.VecDense {
+	agentActions := make([]*mat.VecDense, len(agents))
+	for agentIndex := range agents {
+		agentActions[agentIndex] = agents[agentIndex].GetAction(stateVector)
 	}
+	return agentActions
 }
