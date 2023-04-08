dd1b61a3a1bd1121a7014fde37c9419bada27adb
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-09 02:42:50 +1200

    Rename multiAgentTest to multiAgentSystem

--- cmd/main/multiAgentSystem/multiAgentTestSystem.go
+++ cmd/main/multiAgentSystem/multiAgentTestSystem.go
@@ -1,4 +1,4 @@
-package multiagenttest
+package multiagentsystem
 
 import (
 	agent "OCSS/FoosballGeneticLearning/pkg/Agent"
@@ -13,23 +13,23 @@ const (
 	NUM_AGENTS_PER_SIMULATION = 10
 )
 
-type TestSystem struct {
+type MultiAgentSystem struct {
 }
 
-func (system *TestSystem) NumPercepts() int {
+func (system *MultiAgentSystem) NumPercepts() int {
 	return NUM_PERCEPTS
 }
-func (system *TestSystem) NumActions() int {
+func (system *MultiAgentSystem) NumActions() int {
 	return NUM_ACTIONS
 }
-func (system *TestSystem) NumAgentsPerSimulation() int {
+func (system *MultiAgentSystem) NumAgentsPerSimulation() int {
 	return NUM_AGENTS_PER_SIMULATION
 }
 
 // Returns the initial state of the system
 //
 // In this case it is very boring - the state is always 1.0 flat across all percepts
-func (system *TestSystem) InitializeState() *systemstate.SystemState {
+func (system *MultiAgentSystem) InitializeState() *systemstate.SystemState {
 	return &systemstate.SystemState{
 		StateVector: mat.NewVecDense(system.NumPercepts(), []float64{1.0, 1.0, 1.0, 1.0, 1.0}),
 	}
@@ -44,7 +44,7 @@ func (system *TestSystem) InitializeState() *systemstate.SystemState {
 //
 // The implementation here is very boring - we immediately call the simulation done (terminal = true)
 // and set the score of the agent to the sum of the action vector. Importantly, we do this for EACH AGENT
-func (system *TestSystem) AdvanceState(state *systemstate.SystemState, agents []*agent.Agent) {
+func (system *MultiAgentSystem) AdvanceState(state *systemstate.SystemState, agents []*agent.Agent) {
 	agentActions := agent.GetAllAgentActions(agents, state.StateVector)
 	state.TerminalState = true
 	for agentIndex := range agents {
