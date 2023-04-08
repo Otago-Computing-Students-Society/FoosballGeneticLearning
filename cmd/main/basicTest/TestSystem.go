8e3ef9465d0e81946ce64a3befc154281d48d174
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-09 02:16:45 +1200

    Add documentation of basic test system

--- cmd/main/basicTest/TestSystem.go
+++ cmd/main/basicTest/TestSystem.go
@@ -26,12 +26,25 @@ func (system *TestSystem) NumAgentsPerSimulation() int {
 	return NUM_AGENTS_PER_SIMULATION
 }
 
+// Returns the initial state of the system
+//
+// In this case it is very boring - the state is always 1.0 flat
 func (system *TestSystem) InitializeState() *systemstate.SystemState {
 	return &systemstate.SystemState{
-		StateVector: mat.NewVecDense(system.NumPercepts(), []float64{1.0}),
+		StateVector:   mat.NewVecDense(system.NumPercepts(), []float64{1.0}),
+		TerminalState: false,
 	}
 }
 
+// Defines the behavior of the system
+//
+// This method should take the current system state and agents, find the agent actions,
+// then apply those actions to the state to update it.
+//
+// This method is also responsible for updating the agent scores!
+//
+// The implementation here is very boring - we immediately call the simulation done (terminal = true)
+// and set the score of the agent to the sum of the action vector.
 func (system *TestSystem) AdvanceState(state *systemstate.SystemState, agents []*agent.Agent) {
 	agentActions := agent.GetAllAgentActions(agents, state.StateVector)
 	state.TerminalState = true
