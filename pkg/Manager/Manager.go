59c82b65701c6a77c2c1c63e4e894a2e1dc1ddcb
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-09 02:11:16 +1200

    Implementation of SimulateGeneration method

--- pkg/Manager/Manager.go
+++ pkg/Manager/Manager.go
@@ -65,3 +65,42 @@ func NewManager(system system.System, numSimulationsPerGeneration int) *Manager 
 		generationEndDataCollector:  datacollector.NewGenerationEndCollector(DATA_DIRECTORY),
 	}
 }
+
+// Simulate a single generation of the system, updating the data writers and breeding the next generation
+func (manager *Manager) SimulateGeneration() {
+	defer func() { manager.generationIndex += 1 }()
+	manager.logger.Printf("STARTING SIMULATION OF GENERATION %v\n", manager.generationIndex)
+	numAgentsPerSimulation := manager.system.NumAgentsPerSimulation()
+
+	// Keep a waitgroup to keep track of how many simulations have finished
+	var simulationWaitGroup sync.WaitGroup
+	// Actually start all the simulations
+	for simulationIndex := 0; simulationIndex < manager.numSimulationsPerGeneration; simulationIndex++ {
+		simulationWaitGroup.Add(1)
+		// Find the agents to be used in this simulation
+		simulationAgents := manager.currentGeneration[numAgentsPerSimulation*simulationIndex : numAgentsPerSimulation*(simulationIndex+1)]
+		// Start simulation in very simple anonymous wrapper - to decrement waitgroup when simulation is done
+		go func() {
+			defer simulationWaitGroup.Done()
+			simulator.SimulateSystem(manager.system, simulationAgents)
+		}()
+	}
+	simulationWaitGroup.Wait()
+	manager.logger.Println("FINISHED SIMULATING GENERATION")
+
+	// Find the best agent by score
+	bestAgent := manager.currentGeneration[0]
+	for _, agent := range manager.currentGeneration {
+		if bestAgent.Score < agent.Score {
+			bestAgent = agent
+		}
+	}
+	manager.logger.Printf("BEST AGENT SCORE: %v\n", bestAgent.Score)
+
+	// Put these scores into parquet files
+	manager.generationEndDataCollector.CollectGenerationEndData(manager.currentGeneration)
+	manager.bestAgentDataCollector.CollectBestAgentData(bestAgent)
+
+	manager.currentGeneration = manager.geneticBreeder.NextGeneration(manager.currentGeneration)
+	manager.logger.Printf("--------------------------------------------------------------------------------")
+}
