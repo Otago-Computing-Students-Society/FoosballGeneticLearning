c45719f081da86e91bad6ab76632d44fe4004ce7
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-09 02:00:11 +1200

    Update Simulator to remove use of AgentAction or AgentScore

--- pkg/Simulator/Simulator.go
+++ pkg/Simulator/Simulator.go
@@ -10,7 +10,6 @@ const MAXIMUM_SIMULATION_ITERATIONS = 1000000
 // Simulate the given system until the state is found to be terminal
 func SimulateSystem(system system.System, agents []*agent.Agent) {
 	state := system.InitializeState()
-	agentActions := make([]agent.AgentAction, len(agents))
 
 	// Loop forever (until very large value)
 	// or until the state is found to be terminal
@@ -19,10 +18,6 @@ func SimulateSystem(system system.System, agents []*agent.Agent) {
 			break
 		}
 
-		for agentIndex := range agents {
-			agentActions[agentIndex] = agents[agentIndex].GetAction(state.StateVector)
-		}
-
-		state = system.AdvanceState(state, agentActions)
+		system.AdvanceState(state, agents)
 	}
 }
