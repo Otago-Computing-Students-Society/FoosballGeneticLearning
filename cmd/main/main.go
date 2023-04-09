18ebd92119ba28c34a64f997a431ec64fcbce6d6
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-09 16:33:20 +1200

    Update test files to fit new genetic breeder dependency injection

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"math"
+	"time"
+
+	"golang.org/x/exp/rand"
+
 	sys "github.com/Otago-Computer-Science-Society/FoosballGeneticLearning/cmd/main/multiAgentSystem"
+	geneticbreeder "github.com/Otago-Computer-Science-Society/FoosballGeneticLearning/pkg/GeneticBreeder"
 	manager "github.com/Otago-Computer-Science-Society/FoosballGeneticLearning/pkg/Manager"
 )
 
 func main() {
 	targetSystem := sys.MultiAgentSystem{}
-	manager := manager.NewManager(&targetSystem, 100, true)
+
+	geneticBreeder := geneticbreeder.NewGeneticBreeder(
+		rand.NewSource(uint64(time.Now().Nanosecond())),
+		[]float64{0.0, 0.0, 0.5, 0.5},
+		[]float64{0.0, 0.0, 0.0, 0.2, 0.2, 0.2, 0.2, 0.2},
+		math.Pow10(-6))
+	manager := manager.NewManager(&targetSystem, 100, geneticBreeder, true)
 	for generationIndex := 0; generationIndex < 1000; generationIndex++ {
 		manager.SimulateGeneration()
 	}
