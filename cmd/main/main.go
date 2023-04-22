b88687d4382ba43f9f8447708b765042668112a2
Hayden McAlister <haydenmcalister49@gmail.com>
2023-04-22 23:49:14 +1200

    Update main to use new function signatures

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -20,7 +20,7 @@ func main() {
 		[]float64{0.0, 1.0},
 		1,
 		math.Pow10(-6))
-	manager.SimulateManyGenerations(50, 10)
 	manager := manager.NewManager(targetSystem, 100, 16, geneticBreeder, true)
+	manager.SimulateManyGenerations(50)
 	manager.WriteStop()
 }
