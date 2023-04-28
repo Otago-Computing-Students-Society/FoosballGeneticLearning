5d602d28e8f6364a84b8d15a0a846b93c0592e6b
Hayden McAlister <mcaha814@student.otago.ac.nz>
2023-04-28 16:51:22 +1200

    Update main to use this simulation

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,21 +6,21 @@ import (
 
 	"golang.org/x/exp/rand"
 
-	pongsystem "github.com/Otago-Computer-Science-Society/FoosballGeneticLearning/cmd/main/pongSystem"
+	flyingagents "github.com/Otago-Computer-Science-Society/FoosballGeneticLearning/cmd/main/flyingAgents"
 	geneticbreeder "github.com/Otago-Computer-Science-Society/FoosballGeneticLearning/pkg/GeneticBreeder"
 	manager "github.com/Otago-Computer-Science-Society/FoosballGeneticLearning/pkg/Manager"
 )
 
 func main() {
-	targetSystem := pongsystem.NewPongSystem()
+	targetSystem := flyingagents.NewFlyingAgentSystem()
 
 	geneticBreeder := geneticbreeder.NewGeneticBreeder(
 		rand.NewSource(uint64(time.Now().Nanosecond())),
-		[]float64{0.0, 0.0, 1.0, 1.0},
-		[]float64{0.0, 1.0},
+		[]float64{0.0, 0.0, 1.0, 1.0, 1.0},
+		[]float64{0.0, 1.0, 1.0, 1.0},
 		1,
 		math.Pow10(-6))
-	manager := manager.NewManager(targetSystem, 100, 16, geneticBreeder, true)
+	manager := manager.NewManager(targetSystem, 1000, 100, 16, geneticBreeder, true)
 	manager.SimulateManyGenerations(50)
 	manager.WriteStop()
 }
